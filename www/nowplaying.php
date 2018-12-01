ec0bbb73e8047dafe1a42e043ede8efa55ca25a2
tortoise <tortoisewrath@tortoisewrath.com>
2018-11-30 21:04:48 -0600

    some css

--- www/nowplaying.php
+++ www/nowplaying.php
@@ -9,6 +9,24 @@ if(!isset($_SESSION['username'])) {
 <!DOCTYPE html>
 <title>Now Playing</title>
 <link rel="stylesheet" href="style.css">
+<h1>Now Playing</h1>
+<nav class="me">
+	<a href="me.php">
+		<img src="me.png" alt="Me">
+		<br>
+		Me
+	</a>
+	<svg height="0">
+		<filter id="mef">
+		    <feColorMatrix
+		      type="matrix"
+		      values="1 0 0 0.32 0
+		              0 1 0 0 0
+		              0 0 1 0 0
+		              0 0 0 1 0 "/>
+		</filter>
+	</svg>
+</nav>
 <ul class="nowplaying">
 	<?php
 	require_once('config.php');
