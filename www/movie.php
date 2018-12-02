767f463554d169cb3ff9addc0c57db66efe3835d
tortoise <tortoisewrath@tortoisewrath.com>
2018-12-02 16:04:26 -0600

    updated movie page

--- www/movie.php
+++ www/movie.php
@@ -5,6 +5,7 @@ $title = $_GET['title'];
 ?>
 <title><?=$_GET['title']?></title>
 <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <h1><?=$_GET['title']?></h1>
 <?php
 	require_once('config.php');
@@ -17,9 +18,28 @@ $title = $_GET['title'];
 		die('Movie not found!');
 	}
 ?>
-Release date: <?=$releaseDate?><br>
-Rating: <?=$rating?><br>
-Length: <?=$length?><br>
-Genre: <?=$genre?><br>
-Reviews: <?=$reviews?><br>
-AvgScore: <?=$avgScore?><br>
+<aside>
+	<p class="released">Released: <span><?=$releaseDate?></span>
+	<p class="rating"><?=$rating?>
+	<p class="length"><?=($length>60?(floor($length/60).' hr '):'').($length%60).' min'?>
+	<p class="genre"><?=$genre?>
+	<p class="score"><a href="review.php?title=<?=urlencode($_GET['title'])?>"><?php
+		$roundedScore = round($avgScore * 2) / 2;
+		for($i = 1; $i <= $roundedScore; $i++) {
+			echo '<i class="fas fa-star"></i>';
+			$roundedScore--;
+		}
+		if($roundedScore % 1 > 0) {
+			echo '<i class="fas fa-star-half-alt"></i>';
+		}
+		for($i = ceil($roundedScore); $i <= 5; $i++) {
+			echo '<i class="far fa-star"></i>';
+		}
+	?></a>
+	<p class="reviews"><a href="review.php?title=<?=urlencode($_GET['title'])?>"><?=number_format($reviews).' Fan Rating'.($reviews==1?'':'s')?></a>
+</aside>
+<nav>
+	<button formaction="overview.php?title=<?=urlencode($_GET['title'])?>">Overview</button>
+	<button formaction="review.php?title=<?=urlencode($_GET['title'])?>">Movie Review</button>
+	<button formaction="choosetheater.php?title=<?=urlencode($_GET['title'])?>">Choose Theater</button>
+</nav>
