d96e603c53ea552ae26088ea382f8174129a55c8
Sean Gillen <sdgillen@crimson.ua.edu>
2018-12-02 20:04:54 -0600

    finish up order history

--- www/orderhistory.php
+++ www/orderhistory.php
@@ -17,51 +17,52 @@ if(!isset($_SESSION['username'])) {
   <body>
     <h1>Order History</h1>
 
-    <!-- TODO: Search -->
-    <div>
-      <span>Order ID</span>
-      <input id="order_id">
-      <button>Search</button>
-    </div>
+	<form>
+	    <div>
+	      <span>Order ID</span>
+	      <input name="q"<?=isset($_GET['orderid'])?' value="'.$_GET['orderid'].'"':''?>>
+	      <button>Search</button>
+	    </div>
 
-    <table class="order-history__table" border="1">
-      <tr>
-        <th>Select</th>
-        <th>Order ID</th>
-        <th>Movie</th>
-        <th>Status</th>
-        <th>Total Cost</th>
-      </tr>
+	    <table class="order-history__table" border="1">
+	      <tr>
+	        <th>Select</th>
+	        <th>Order ID</th>
+	        <th>Movie</th>
+	        <th>Status</th>
+	        <th>Total Cost</th>
+	      </tr>
 
-      <?php
-      require_once('config.php');
+	      <?php
+	      require_once('config.php');
 
-      $query = $db->prepare('SELECT orderID, title, status, totalTickets FROM ORDERS where username=?');
-      $query->bind_param('s', $_SESSION['username']);
-      $query->execute();
-      $orderID = $title = $status = $totalTickets = NULL;
-      $query->bind_result($orderID, $title, $status, $totalTickets);
-      while($query->fetch()):?>
-        <tr>
-          <td><input type="radio" name="selectedOrder" value=<?=$orderID?>></td>
-          <td><?=$orderID?></td>
-          <td><?=$title?></td>
-          <td><?=$status?></td>
-          <td><?=$totalTickets?></td>
-        </tr>
-      <?php endwhile; ?>
+		  $query = 'SELECT orderID, title, status, totalTickets FROM ORDERS where username=?';
+		  if(isset($_GET['q'])) {
+			  $query .= ' AND orderID=?';
+		      $query = $db->prepare($query);
+		      $query->bind_param('ss', $_SESSION['username'], $_GET['q']);
+	  	  }
+		  else {
+			  $query = $db->prepare($query);
+			  $query->bind_param('s', $_SESSION['username']);
+		  }
+	      $query->execute();
+	      $orderID = $title = $status = $totalTickets = NULL;
+	      $query->bind_result($orderID, $title, $status, $totalTickets);
+	      while($query->fetch()):?>
+	        <tr>
+	          <td><input type="radio" name="orderID" value=<?=$orderID?>></td>
+	          <td><?=$orderID?></td>
+	          <td><?=$title?></td>
+	          <td><?=$status?></td>
+	          <td><?=$totalTickets?></td>
+	        </tr>
+	      <?php endwhile;
+		  $query->close(); ?>
 
-    </table>
+	    </table>
 
-    <button onclick="viewDetail(event)" >View Detail</button>
+	    <button formaction="viewdetail.php" >View Detail</button>
+	</form>
   </body>
-
-  <script>
-    function viewDetail() {
-      const selectedOrder = document.querySelector('input[name=selectedOrder]:checked')
-      if (selectedOrder) {
-        window.location = `viewdetail.php?orderID=${selectedOrder.value}`
-      }
-    }
-  </script>
 </html>
