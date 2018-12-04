9b1cac16db06f4223def13a1ce5cef4dfafb1275
tortoise <sdgillen@crimson.ua.edu>
2018-12-04 15:58:01 -0600

    CSS for Overview page

--- www/overview.php
+++ www/overview.php
@@ -6,7 +6,7 @@ $title = $_GET['title'];
 <title><?=$_GET['title']?></title>
 <link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-<h1><?=$_GET['title']?></h1>
+<h2><?=$_GET['title']?></h2>
 <?php
     require_once('config.php');
 	$query = $db->prepare('SELECT synopsis, cast FROM MOVIE WHERE MOVIE.title = ?');
@@ -18,10 +18,10 @@ $title = $_GET['title'];
 		die('Movie not found!');
 	}
 ?>
-<aside>
-	<p class="synopsis">Synopsis <br> <?=$synopsis?>
-	<p class="cast">Cast <br> <?=$cast?>
-</aside>
+<article>
+	<h3 class="synopsis">Synopsis</h3><p class="synopsis"> <?=$synopsis?>
+	<h3 class="cast">Cast</h3><p class="cast"> <?=$cast?>
+</article>
 <form method="GET">
 	<input type="hidden" name="title" value="<?=$_GET['title']?>">
 	<button formaction="movie.php">Back</button>
