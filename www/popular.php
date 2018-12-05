bb7232b2461124d012ddaaf13dca7a80da91a0dc
tortoise <sdgillen@crimson.ua.edu>
2018-12-04 18:47:08 -0600

    minor changes to calculations in popular & revenue

--- www/popular.php
+++ www/popular.php
@@ -18,19 +18,26 @@ if(!@$_SESSION['manager']) {
 
 <h1>View Popular Movie Report</h1>
 
-<form>
-	<table class="order-history__table" border="1">
-	      <tr>
-	        <th>Month</th>
-	        <th>Movie</th>
-	        <th>#of Orders</th>
-	      </tr>
-	<?php
-	require_once('config.php');
-	$query = $db->prepare('SELECT count(*) as total, title, month(date) as month FROM orders as q1 WHERE q1.status <> "cancelled" and month(date) = "12" GROUP BY month(date), title ORDER BY total DESC LIMIT 3');
+<table class="popular">
+      <tr>
+        <th>Month</th>
+        <th>Movie</th>
+        <th>#of Orders</th>
+      </tr>
+<?php
+require_once('config.php');
+
+for($month = intval(date('n')) - 2; $month <= intval(date('n')); $month++):
+
+	$year = intval(date('Y'));
+	if($month > intval(date('n'))) $year--;
+
+	echo '<tbody>';
+	$query = $db->prepare('SELECT count(*) AS total, title as month FROM ORDERS AS q1 WHERE q1.status <> "cancelled" AND month(date) = ? AND year(date) = ? GROUP BY month(date), title ORDER BY total DESC LIMIT 3');
+	$query->bind_param('ii', $month, $year);
 	$query->execute();
-	$title = $count = NULL;
-	$query->bind_result($total, $title, $month);
+	$title = $total = NULL;
+	$query->bind_result($total, $title);
 	 while($query->fetch()):
 	 	$monthName = date("F", mktime(0, 0, 0, $month, 10));
 	 	?>
@@ -40,39 +47,11 @@ if(!@$_SESSION['manager']) {
 	          <td><label for="order<?=$ticketPrice?>"><?=$total?></label></td>
 		  </tr></label>
 	      <?php endwhile;
-		  $query->close(); ?>
-		  <?php
-	$query = $db->prepare('SELECT count(*) as total, title, month(date) as month FROM orders as q1 WHERE q1.status <> "cancelled" and month(date) = "11" GROUP BY month(date), title ORDER BY total DESC LIMIT 3');
-	$query->execute();
-	$title = $count = NULL;
-	$query->bind_result($total, $title, $month);
-	 while($query->fetch()):
-	 	$monthName = date("F", mktime(0, 0, 0, $month, 10));
-	 	?>
-	        <tr>
-	          <td><label for="order<?=$month?>"><?=$monthName?></label></td>
-	          <td><label for="order<?=$ticketPrice?>"><?=$title?></label></td>
-	          <td><label for="order<?=$ticketPrice?>"><?=$total?></label></td>
-		  </tr></label>
-	      <?php endwhile;
-		  $query->close(); ?>
-		    <?php
-	$query = $db->prepare('SELECT count(*) as total, title, month(date) as month FROM orders as q1 WHERE q1.status <> "cancelled" and month(date) = "10" GROUP BY month(date), title ORDER BY total DESC LIMIT 3');
-	$query->execute();
-	$title = $count = NULL;
-	$query->bind_result($total, $title, $month);
-	 while($query->fetch()):
-	 	$monthName = date("F", mktime(0, 0, 0, $month, 10));
-	 	?>
-	        <tr>
-	          <td><label for="order<?=$month?>"><?=$monthName?></label></td>
-	          <td><label for="order<?=$ticketPrice?>"><?=$title?></label></td>
-	          <td><label for="order<?=$ticketPrice?>"><?=$total?></label></td>
-		  </tr></label>
-	      <?php endwhile;
-		  $query->close(); ?>
+		  $query->close();
+
+	echo '</tbody>';
+endfor;?>
 </table>
-</form>
 
 <form>
 	<button formaction="managerview.php">Back</button>
--- www/revenue.php
+++ www/revenue.php
@@ -8,52 +8,43 @@ if(!@$_SESSION['manager']) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-  <header>
     <title>View Revenue Report</title>
     <link rel="stylesheet" href="style.css">
-  </header>
 
-<body>
 
 <h1>View Revenue Report</h1>
 
-<form>
-	<table class="order-history__table" border="1">
-	      <tr>
-	        <th>Month</th>
-	        <th>Revenue</th>
-	      </tr>
-	<?php
-	require_once('config.php');
-	$query = $db->prepare('SELECT childDiscount, seniorDiscount, cancellationFee FROM SYSTEMINFO');
-		  $childDiscount = $seniorDiscount = $cancellationFee = NULL;
-		  $query->bind_result($childDiscount, $seniorDiscount, $cancellationFee);
-		  $query->execute();
-		  $query->fetch();
-		  $query->close();
-	$query = $db->prepare('SELECT month(date), SUM(adulttickets) as adulttickets, SUM(childtickets) as childtickets, SUM(seniortickets) as seniorTickets, ticketprice FROM ORDERS GROUP BY month(date) DESC LIMIT 3');
-	$query->execute();
-	$month = $adulttickets = $childtickets = $seniortickets = $ticketprice = NULL;
-	$query->bind_result($month, $adulttickets, $childtickets, $seniortickets, $ticketprice);
-	 while($query->fetch()):
-	 	$monthName = date("F", mktime(0, 0, 0, $month, 10));
-	 	 $childPrice = $ticketprice * (100 - $childDiscount) / 100;
-			  $seniorPrice = $ticketprice * (100 - $seniorDiscount) / 100;
-			  $totalPrice = $ticketprice * $adulttickets + $childPrice * $childtickets + $seniorPrice * $seniortickets;
-	 	?>
-	        <tr>
-	          <td><label for="order<?=$month?>"><?=$monthName?></label></td>
-	          <td><label for="order<?=$ticketPrice?>"><?=$totalPrice?></label></td>
-		  </tr></label>
-	      <?php endwhile;
-		  $query->close(); ?>
+<table class="revenue">
+      <tr>
+        <th>Month</th>
+        <th>Revenue</th>
+      </tr>
+<?php
+require_once('config.php');
+$query = $db->prepare('SELECT childDiscount, seniorDiscount FROM SYSTEMINFO');
+	  $childDiscount = $seniorDiscount = NULL;
+	  $query->bind_result($childDiscount, $seniorDiscount);
+	  $query->execute();
+	  $query->fetch();
+	  $query->close();
+$query = $db->prepare('SELECT month(date), SUM(adultTickets * ticketPrice), SUM(childTickets * ticketPrice), SUM(seniorTickets * ticketPrice) FROM ORDERS GROUP BY month(date) ORDER BY month(date) DESC LIMIT 3');
+$query->execute();
+$month = $adulttickets = $childtickets = $seniortickets = NULL;
+$query->bind_result($month, $adulttickets, $childtickets, $seniortickets);
+ while($query->fetch()):
+ 	$monthName = date("F", mktime(0, 0, 0, $month, 10));
+ 	 $childPrice = (100 - $childDiscount) / 100;
+		  $seniorPrice = (100 - $seniorDiscount) / 100;
+		  $totalPrice = $adulttickets + $childPrice * $childtickets + $seniorPrice * $seniortickets;
+ 	?>
+        <tr>
+          <td><?=$monthName?></td>
+          <td>$<?=$totalPrice?></td>
+	  </tr>
+      <?php endwhile;
+	  $query->close(); ?>
 </table>
-</form>
 
 <form>
 	<button formaction="managerview.php">Back</button>
 </form>
-
-  </body>
-</html>
