6587705392e4e42aa6a3c2b7025d98ff56990a23
tortoise <sdgillen@crimson.ua.edu>
2018-12-05 08:48:33 -0600

    updates to give review

--- www/givereview.php
+++ www/givereview.php
@@ -17,8 +17,34 @@ $query->execute();
 $query->fetch();
 $query->close();
 
-if($count < 1){
-    die("You have not seen this movie");
+$emptyComment = 0;
+
+if(isset($_POST['title']) && $count >= 1){
+	if(empty(trim($_POST['comment']))) {
+		$emptyComment = 1;
+	}
+	else {
+	    $query2 = $db->prepare('SELECT reviewID FROM REVIEW ORDER BY cast(reviewID as unsigned) DESC LIMIT 1');
+	    $reviewID = NULL;
+	    $query2->bind_result($reviewID);
+	    $query2->execute();
+	    $query2->fetch();
+	    $query2->close();
+	    $reviewID = intval($reviewID)+1;
+
+	    $comment = $_POST["comment"];
+	    $reviewTitle = $_POST["reviewTitle"];
+	    $rating = $_POST["rating"];
+
+	    $insert = $db->prepare('INSERT INTO REVIEW VALUES (?,?,?,?,?,?)');
+	    $insert->bind_param("ssssis", $reviewID, $reviewTitle, $comment, $_GET['title'], $rating, $_SESSION['username']);
+	    if(!$insert->execute()) {
+	        die($db->errno . ' (' . $db->error . ')');
+	    }
+	    $insert->close();
+		header('Location: review.php?'.http_build_query($_GET));
+		die();
+	}
 }
 
 ?>
@@ -33,50 +59,33 @@ $title = $_GET['title'];
 <body class="givereviews">
 <h1><?=$_GET['title']?></h1>
 <form method="POST">
-    <label>Rating</label>
-    <select name="rating">
-        <option value=1>1</option>
-        <option value=2>2</option>
-        <option value=3>3</option>
-        <option value=4>4</option>
-        <option value=5>5</option>
-    </select>
-    <br>
-    <label>Title</label>
-	<input name="reviewTitle" type="text" id="reviewTitle"/>
-    <br>
-    <label>Comment</label>
-    <input name="comment" type="text" id="comment">
-    <br>
-	<input type="submit" name="submit" value="Submit"/>
-</form>
+	<?php
 
-<form method="GET">
-	<input type="hidden" name="title" value="<?=$_GET['title']?>">
-	<button formaction="review.php">Back</button>
+	if($count < 1): ?>
+		<p class="formError">You have not seen this movie! You must buy a ticket and watch the movie before you can review it.</p>
+	<?php else: ?>
+		<input type="hidden" name="title" value="<?=$_GET['title']?>">
+	    <label>Rating</label>
+	    <select name="rating">
+	        <option value=1<?=@$_POST['rating']==='1'?' selected':''?>>1</option>
+	        <option value=2<?=@$_POST['rating']==='2'?' selected':''?>>2</option>
+	        <option value=3<?=@$_POST['rating']==='3'?' selected':''?>>3</option>
+	        <option value=4<?=@$_POST['rating']==='4'?' selected':''?>>4</option>
+	        <option value=5<?=@$_POST['rating']==='5'?' selected':''?>>5</option>
+	    </select>
+	    <br>
+	    <label>Title</label>
+		<input name="reviewTitle" type="text" id="reviewTitle" value="<?=@$_POST['reviewTitle']?>"/>
+	    <br>
+		<?php if($emptyComment): ?>
+			<p class="formError">You must type a review</p>
+		<?php endif; ?>
+	    <label>Comment</label>
+	    <textarea name="comment" id="comment"><?=@$_POST['comment']?></textarea>
+	    <br>
+		<button>Submit</button>
+		<div id="gap"></div>
+		<button formaction="review.php" formmethod="GET">Back</button>
+	<?php endif; ?>
 </form>
 </body>
-
-<?php
-    require_once('config.php');
-    if(isset($_POST['submit'])){
-        $query2 = $db->prepare('SELECT MAX(reviewID) FROM REVIEW');
-        $reviewID = NULL;
-        $query2->bind_result($reviewID);
-        $query2->execute();
-        $query2->fetch();
-        $query2->close();
-        $reviewID = $reviewID+1;
-
-        $comment = $_POST["comment"];
-        $reviewTitle = $_POST["reviewTitle"];
-        $rating = $_POST["rating"];
-
-        $insert = $db->prepare('INSERT INTO REVIEW VALUES (?,?,?,?,?,?)');
-        $insert->bind_param("ssssis", $reviewID, $reviewTitle, $comment, $_GET['title'], $rating, $_SESSION['username']);
-        if(!$insert->execute()) {
-            die($db->errno . ' (' . $db->error . ')');
-        }
-        $insert->close();
-    }
-?>
