6cc8ddab27fdc4114a211456b4f563f590c74a7e
tortoise <sdgillen@crimson.ua.edu>
2018-12-04 19:01:56 -0600

    CSS and updated semantics for popular movie report

--- www/popular.php
+++ www/popular.php
@@ -8,31 +8,28 @@ if(!@$_SESSION['manager']) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-  <header>
     <title>View Popular Movie Report</title>
     <link rel="stylesheet" href="style.css">
-  </header>
-
-<body>
 
 <h1>View Popular Movie Report</h1>
 
 <table class="popular">
+	<thead>
       <tr>
         <th>Month</th>
         <th>Movie</th>
         <th>#of Orders</th>
       </tr>
+  </thead>
 <?php
 require_once('config.php');
 
 for($month = intval(date('n')) - 2; $month <= intval(date('n')); $month++):
-
+	$first = 1;
 	$year = intval(date('Y'));
 	if($month > intval(date('n'))) $year--;
 
-	echo '<tbody>';
+
 	$query = $db->prepare('SELECT count(*) AS total, title as month FROM ORDERS AS q1 WHERE q1.status <> "cancelled" AND month(date) = ? AND year(date) = ? GROUP BY month(date), title ORDER BY total DESC LIMIT 3');
 	$query->bind_param('ii', $month, $year);
 	$query->execute();
@@ -41,21 +38,17 @@ for($month = intval(date('n')) - 2; $month <= intval(date('n')); $month++):
 	 while($query->fetch()):
 	 	$monthName = date("F", mktime(0, 0, 0, $month, 10));
 	 	?>
-	        <tr>
-	          <td><label for="order<?=$month?>"><?=$monthName?></label></td>
-	          <td><label for="order<?=$ticketPrice?>"><?=$title?></label></td>
-	          <td><label for="order<?=$ticketPrice?>"><?=$total?></label></td>
-		  </tr></label>
-	      <?php endwhile;
+	        <tr<?=$first?' class="first"':''?>>
+	          <td><?=$monthName?></td>
+	          <td><?=$title?></td>
+	          <td><?=$total?></td>
+		  </tr>
+	      <?php $first = 0; endwhile;
 		  $query->close();
 
-	echo '</tbody>';
 endfor;?>
 </table>
 
 <form>
 	<button formaction="managerview.php">Back</button>
 </form>
-
-  </body>
-</html>
